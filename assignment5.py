d4aaff2b583b2a36408272665d29720eef2db4b5
Sophia Trump <strump@omega.cs.brynmawr.edu>
2018-11-30 12:28:54 -0500

    Implemented basic CFG (not in CNF) with print output

---
+++ assignment5.py
@@ -0,0 +1,69 @@
+# Name: Sophia Trump
+# File: htToTokens.py
+# Description: Prepares the NLTK, LINUX, and "FRUGAL" dictionaries.
+#              Each function returns the respective dictionary as a list of words.
+# Last modified: Thurs, 11 October 2018
+
+import nltk
+import urllib.request
+import re
+
+def main():
+    # create the CFG
+    grammar = nltk.CFG.fromstring(
+   """
+   S -> WH_NP VP
+   S -> WH_NP AUXV NP VP
+   S -> NP VP
+   NP -> N | PRON | PROPN | AUXV NP | PREDET N | Det Nominal | Det Nominal PP 
+   VP -> V | V ADJ | V WH_NP| V NP | V NP PP 
+   PREDET -> Det | PREDET Det
+   Nominal -> N | Nominal N
+   PP -> P NP
+   WH_NP -> WH_PRON
+   WH_PRON -> "who" | "what"
+   PRON -> "i"
+   AUXV -> "does"
+   PROPN -> "socrates" | "deepak" | "rover" | "snoopy" | "tweety" | "polly"
+   ADJ -> "human" | "biped"
+   V -> "saw" | "ate" | "is" | "has" | "swims" | "flies" | "fly" | "are" | "have" | "bite" | "bites" | "walk" | "walks" | "does"
+   Det -> "a" | "the" | "an" | "all"
+   N -> "man" | "dog" | "dogs" | "telescope" | "feathers" | "hair" | "park" | "collies" | "collie" | "beagles" | "beagle" | "parrots" | "parrot" | "canaries" | "canary" | "fish" | "animal" | "animals" | "bird" | "birds" | "gills" | "mammals" | "humans" | "canines"
+   P -> "in" | "under" | "with"
+   """)
+
+    # create the parser
+    RDparser = nltk.RecursiveDescentParser(grammar)
+    
+    # get the sentences 
+    sentences = getSents()
+
+    # tag each word in each sentence
+    parsedSents = []
+    for sent in sentences:
+        for p in RDparser.parse_one(sent):
+            print(p)
+        print("\n")
+       
+    
+
+
+
+def getSents():
+    sents = []
+    
+    page_source = urllib.request.urlopen('https://cs.brynmawr.edu/Courses/cs325/fall2018/test2.txt').read().decode('utf-8')
+    lines = re.sub('\r|\?|\.|\n', '\n', page_source)
+    lines = re.split('\n', lines)
+    for l in lines:
+        if l:
+           l = l.lower()
+           words = nltk.word_tokenize(l)
+           sents.append(words)
+    return sents
+
+
+
+
+
+    
